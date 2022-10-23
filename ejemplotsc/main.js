b8b009a1a15c2f2219719c52f6b1e2c95ad3a2b8
samuelfreire03 <88785120+samuelfreire03@users.noreply.github.com>
2022-10-22 21:24:58 -0500

    taller typescript 1

---
+++ ejemplotsc/main.js
@@ -0,0 +1,38 @@
+import { Series } from './Series.js';
+let Tabla = document.getElementById("AquiSeries");
+let promedio = 0;
+for (let item of Series) {
+    let fila = document.createElement('tr');
+    let fila_id = document.createElement('td');
+    fila_id.innerHTML = (item.idSerie).toString();
+    let fila_nombre = document.createElement('td');
+    fila_nombre.innerHTML = (item.nombre).toString();
+    let fila_canal = document.createElement('td');
+    fila_canal.innerHTML = (item.canal).toString();
+    let fila_temp = document.createElement('td');
+    fila_temp.innerHTML = (item.temporadas).toString();
+    fila.appendChild(fila_id);
+    fila.appendChild(fila_nombre);
+    fila.appendChild(fila_canal);
+    fila.appendChild(fila_temp);
+    Tabla.appendChild(fila);
+}
+let suma = 0;
+for (let item of Series) {
+    suma = suma + item.temporadas;
+}
+promedio = suma / Series.length;
+let fila = document.createElement('tr');
+let fila_promedio = document.createElement('td');
+fila_promedio.innerHTML = "Seasons average: " + (promedio).toString();
+let fila_relleno1 = document.createElement('td');
+fila_relleno1.innerHTML = " ";
+let fila_relleno2 = document.createElement('td');
+fila_relleno2.innerHTML = " ";
+let fila_relleno3 = document.createElement('td');
+fila_relleno3.innerHTML = " ";
+fila.appendChild(fila_promedio);
+fila.appendChild(fila_relleno1);
+fila.appendChild(fila_relleno2);
+fila.appendChild(fila_relleno3);
+Tabla.appendChild(fila);
